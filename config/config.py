ca9ea63dd3f369c5c222d2853623e9d20429f596
fredboneML <30954748+fredboneML@users.noreply.github.com>
2020-09-20 22:24:09 +0200

    3rd commit

---
+++ config/config.py
@@ -0,0 +1,24 @@
+
+
+JOB_LIST = ['admin.',
+ 'blue-collar',
+ 'entrepreneur',
+ 'housemaid',
+ 'management',
+ 'retired',
+ 'self-employed',
+ 'services',
+ 'student',
+ 'technician',
+ 'unemployed',
+ 'unknown']
+
+MARITAL_LIST = ['divorced', 'married', 'single']
+
+EDUCATION_LIST = ['primary', 'secondary', 'tertiary', 'unknown']
+
+DEFAULT_LIST = ['no', 'yes']
+
+HOUSING_LIST = ['no', 'yes']
+
+LOAN_LIST = ['no', 'yes']
